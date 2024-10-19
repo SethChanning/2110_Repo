8f4fc28f6656dae390081111d0afa754118ddb2c
Sceti-Seth <sethachanning@icloud.com>
2024-10-19 19:28:08 -0300

    the

--- Lab5_2110/LinkedListDemo.java
+++ Lab5_2110/LinkedListDemo.java
@@ -1,51 +1,69 @@
-import java.util.*;
-import java.util.LinkedList;
-
+//Full Name: Seth Alexander Channing
+//Banner ID: B00956611
+import java.util.Scanner;
 public class LinkedListDemo {
-    public static void Main(String[] args){
-        LinkedList<String> Pokemon= new LinkedList<>();
-        Scanner keyboard= new Scanner(System.in);
-        String pokemon= keyboard.next();;
-        while(!keyboard.equals("quit")){
+    public static void main(String[] args) {
+        LinkedList<String> Pokemon = new LinkedList<>();
+        Scanner keyboard = new Scanner(System.in);
+        String pokemon = keyboard.next();
+        while (!pokemon.equals("quit")) {
             Pokemon.add(pokemon);
-            pokemon= keyboard.next();
+            pokemon = keyboard.next();
         }//creates linkedlist of pokemon
         displayEven(Pokemon);
+        System.out.println("");
         displayOdd(Pokemon);
-        reverse(Pokemon);
-        removeMiddle(Pokemon);
-
-    }
-
-
-
-
-
-    //method to display even-numbered nodes
-    public static void displayEven(LinkedList<String> list){
-        int counter=0;
-        String curr=list.getFirst();
-        while (curr != null) {
-            if (counter % 2 == 0) {
-                System.out.println(curr);
-            }
-            counter++;
-            curr=curr.getNext();
+        System.out.println("");
+        LinkedList<String> reversedPokemon = reverse(Pokemon);
+        for (int i = 0; i < reversedPokemon.size(); i++) {
+            System.out.print(reversedPokemon.getAt(i) + " ");
         }
-
-
-//continue
+        System.out.println("");
+        removeMiddle(Pokemon);
     }
-    //method to display odd-numbered nodes
+    //method to display even-numbered nodes
+    public static void displayEven(LinkedList<String> list) {
+        int index = 0;
+        String curr = list.getAt(index);
+        while (index<list.size()) {
+            curr=list.getAt(index);
+            if (index % 2 == 0) {
+                System.out.print(curr+" ");
+            }
+            index++;
+        }
+    }
+//method to display odd-numbered nodes
     public static void displayOdd(LinkedList<String> list){
-//continue
+        int index = 0;
+        String curr = list.getAt(index);
+        while (index<list.size()) {
+            curr=list.getAt(index);
+            if (index % 2 != 0) {
+                System.out.print(curr+" ");
+            }
+            index++;
+        }
     }
     //method to create a reversed linked list
-    public static LinkedList<String> reverse(LinkedList<String> list){
-//continue
+    public static LinkedList<String> reverse(LinkedList<String> list1) {
+        LinkedList<String> list2 = new LinkedList<>();
+        for (int i = 0; i < list1.size(); i++) {
+            list2.add(list1.getAt(i));
+        }
+        return list2;
     }
-    //method to remove the middle node in the linked list
-    public static void removeMiddle(LinkedList<String> list){
-//continue
+//    //method to remove the middle node in the linked list
+    public static void removeMiddle(LinkedList<String> list) {
+        int middle=list.size()/2;
+        int index=0;
+        String curr=list.getAt(index);
+        list.removeAt(middle);
+        while (index<list.size()) {
+            curr=list.getAt(index);
+            System.out.print(curr+" ");
+            index++;
+        }
     }
+
 }
